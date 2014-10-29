521c6456a74c00f6339758df3c24efe84d72f958
redliontom <tomschalbar@hotmail.com>
2014-10-29 18:49:48 +0100

    Infopage - Webdesign fertig

---
+++ app/app/scripts/signin.js
@@ -0,0 +1,26 @@
+angular.module('phStart', ['ui.router'])
+
+.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('signin', {
+                url: '/signin',
+                templateUrl: 'templates/signin/signIn.html'
+            })
+
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'templates/signin/signUp.html'
+            })
+
+            .state('forgotPassword', {
+                url: '/forgotPassword',
+                templateUrl: 'templates/signin/forgotPassword.html'
+            })
+
+            .state('menu', {
+                url: '/menu',
+                templateUrl: 'templates/signin/menu.html'
+            })
+}]);
