f8f001aceae1564acea323bcfbd6f7f64d7b61c8
redliontom <tomschalbar@hotmail.com>
2014-11-25 14:54:00 +0100

    Update: Sign in

--- app/frontend/scripts/Controllers/signin.js
+++ app/frontend/scripts/Controllers/signin.js
@@ -13,9 +13,19 @@ angular.module('phStart').controller('SinginCtrl', function($scope){
                 contentType: 'application/json',
                 success: function(data)
                 {
+                    console.log(data);
                     console.log("Sign in --> Okay");
                     window.location.replace('/');
                     window.location.reload();
+                },
+                error: function (data) {
+                  console.log(JSON.stringify(data.responseJSON.error));
+                  $('.message').html(JSON.stringify(data.responseJSON.error));
+                  $('.message').css('visibility', 'visible');
+                  setTimeout(function() {
+                    $('.message').css('visibility', 'hidden');
+                  }, 3000);
+                  console.log("Error");
                 }
             });
         }
