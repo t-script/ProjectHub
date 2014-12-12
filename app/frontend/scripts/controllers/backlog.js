07d48b86ede7d9d4b52a91882dd3f65829e18e1d
redliontom <tomschalbar@hotmail.com>
2014-12-12 01:30:33 +0100

    Backlog komplett fertig

--- app/frontend/scripts/controllers/backlog.js
+++ app/frontend/scripts/controllers/backlog.js
@@ -1,12 +1,34 @@
 'use strict';
 
 angular.module('phApp').controller('BacklogCtrl', function($scope, $sails){
+  $scope.tickets = [];
+
+  $scope.showTicket = function() {
+    $('#addNewTicket').hide();
+    $('#addTicket').show();
+  }
+
+  $scope.getTickets = function () {
+    $sails.get('/tickets')
+      .success(function (data) {
+        $scope.tickets = data;
+      })
+      .error(function (data) {
+        console.log(data);
+      })
+  }
+
   $scope.addTicket = function () {
     if ($scope.frmAddTicket.$valid) {
+      $('#ticketTitle').val(null);
+      $('#ticketDescription').val(null);
+      $('#addNewTicket').show();
+      $('#addTicket').hide();
       var data = {ticketid: 1, title: $scope.ticketTitle, description: $scope.ticketDescription };
       $sails.post('/tickets', data)
         .success(function (data) {
           console.log('success');
+          $scope.getTickets();
         })
         .error(function (data) {
           console.log(data)
