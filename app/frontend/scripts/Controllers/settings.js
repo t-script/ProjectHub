6da684718003ad5497757b4804c5e86c750c08d4
redliontom <tomschalbar@hotmail.com>
2015-01-13 22:49:37 +0100

    Frontend: Setting --> Done!

--- app/frontend/scripts/Controllers/settings.js
+++ app/frontend/scripts/Controllers/settings.js
@@ -1,5 +1,32 @@
 'use strict';
 
-angular.module('phApp').controller('SettingsCtrl', function($scope){
+angular.module('phApp').controller('SettingsCtrl', function($scope, $sails){
+  $scope.user = [];
+  $('.remove').hide();
+
+  $scope.getUser = function() {
+    $sails.get('/getUsername')
+      .success(function (data) {
+        $scope.user = data;
+      })
+      .error(function (data) {
+        console.log(data);
+      })
+  }
+
+  $scope.showInput = function (text, input, pencil, remove) {
+    $('#'+text).hide();
+    $('#'+pencil).hide();
+    $('#'+remove).show();
+    $('#'+input).show();
+    $('#'+input).focus();
+  }
+
+  $scope.cancelInput = function (text, input, pencil, remove) {
+    $('#'+text).show();
+    $('#'+pencil).show();
+    $('#'+input).hide();
+    $('#'+remove).hide();
+  }
 
 });
