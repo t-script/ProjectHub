08988cf238e2ab816c69fc29608a330e6528678d
redliontom <tomschalbar@hotmail.com>
2015-01-14 11:24:24 +0100

    Dashboard: Design fertig

--- app/frontend/scripts/Controllers/dashboard.js
+++ app/frontend/scripts/Controllers/dashboard.js
@@ -1,5 +1,8 @@
 'use strict';
 
 angular.module('phApp').controller('DashboardCtrl', ['$scope', function ($scope) {
-
+  $scope.countProject=0;
+  $scope.countTickets=0;
+  $scope.countMembers=0;
+  $scope.countEvents=0;
 }]);
